d2fb9f0eb2b2b110e3567f60f48c3fb2d5baea99
rodri <rodri@Rodri>
2024-04-08 20:30:02 -0300

    ejercicio 2

--- tp1_RodrigoHuerta/src/ar/edu/unju/fi/ejercicio2/main.java
+++ tp1_RodrigoHuerta/src/ar/edu/unju/fi/ejercicio2/main.java
@@ -2,9 +2,27 @@ package ar.edu.unju.fi.ejercicio2;
 
 public class main {
 
-	public static void main(String[] args) {
+
+		    public static void main(String[] args) {
+		       
+		        String nombrePais = "Argentina";
+		        int edadPersona = 18;
+		        double alturaEdificio = 70.45; 
+		        double precioProducto = 20; 
+		        String numeroTelefono = "+1111111111";
+		        double cosenoDe05 = Math.cos(0.5);
+		        
+		        System.out.println("Nombre del país: " + nombrePais);
+		        System.out.println("Edad de una persona: " + edadPersona + " años");
+		        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+		        System.out.println("Precio del producto: $" + precioProducto);
+		        System.out.println("Número de teléfono: " + numeroTelefono);
+		        System.out.println("Coseno de 0.5: " + cosenoDe05);
+		    }
+		
+
 		
 		
 	}
 
-}
+
