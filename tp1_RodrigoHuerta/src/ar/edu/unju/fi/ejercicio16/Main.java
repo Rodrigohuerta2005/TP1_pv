d0159a43a6dbad4b6f8e7da7af7d47c52fc9bb73
rodri <rodri@Rodri>
2024-04-20 17:51:09 -0300

    Ejercicio 16 terminado

---
+++ tp1_RodrigoHuerta/src/ar/edu/unju/fi/ejercicio16/Main.java
@@ -0,0 +1,51 @@
+package ar.edu.unju.fi.ejercicio16;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+
+	  
+	        String[] nombres = new String[5];
+
+	        
+	        for (int i = 0; i < nombres.length; i++) {
+	            System.out.print("Ingrese el nombre de la persona " + (i + 1) + ": ");
+	            nombres[i] = scanner.nextLine();
+	        }
+
+	       
+	        System.out.println("\nValores guardados en el array:");
+	        int indice = 0;
+	        while (indice < nombres.length) {
+	            System.out.println("Posición " + indice + ": " + nombres[indice]);
+	            indice++;
+	        }
+
+	        System.out.println("\nTamaño del array: " + nombres.length);
+
+	       
+	        byte indiceAEliminar;
+	        do {
+	            System.out.print("Ingrese el índice (0-" + (nombres.length - 1) + ") del elemento a eliminar: ");
+	            indiceAEliminar = Byte.parseByte(scanner.nextLine());
+	        } while (indiceAEliminar < 0 || indiceAEliminar >= nombres.length);
+
+	        
+	        for (int i = indiceAEliminar; i < nombres.length - 1; i++) {
+	            nombres[i] = nombres[i + 1];
+	        }
+	        nombres[nombres.length - 1] = null; 
+
+	        
+	        System.out.println("\nValores después de eliminar el elemento:");
+	        for (int i = 0; i < nombres.length; i++) {
+	            System.out.println("Posición " + i + ": " + nombres[i]);
+	        }
+	    }
+	}
+
+
